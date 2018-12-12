fbe76dc3a64afda97e19b6653bab99d3f96fc17b
Guinther Kovalski <43643854+GuintherKovalski@users.noreply.github.com>
2018-12-12 12:16:28 -0200

    Update invpower.m

--- invpower.m
+++ invpower.m
@@ -2,8 +2,10 @@ function [num1 den1] = invpower(num,den)
 if ~isvector(num)
     error('Input must be a vector')
 end
-if ~isvector(den)
-    error('Input must be a vector')
+
+if ~exist('den','var')
+     % third parameter does not exist, so default it to something
+     den=1;
 end
 invp=tf(conv([1 0],fliplr(num)),fliplr(den));
 [num1,den1] = tfdata(invp,'v');  
